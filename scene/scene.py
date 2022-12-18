6aff16dcc974da3ef34a37d01fd1b415bc6c6e68
aastashov <aastashov@atollis.com>
2022-12-18 15:26:19 +0300

    Added functionality to show window to class Scene

--- scene/scene.py
+++ scene/scene.py
@@ -1,3 +1,6 @@
+import pygame
+from pygame.surface import SurfaceType
+
 from camera.camera import Camera
 from model.model import Model3D
 
@@ -10,7 +13,9 @@ class Scene:
         self.__window = self.__initialize_window()
 
     def __initialize_window(self):
-        return []
+        pygame.display.set_caption("CG Lab")
+        pygame.display.set_icon(SurfaceType(size=(0, 0)))
+        return pygame.display.set_mode((self.__camera.get_W(), self.__camera.get_H()))
 
     def __draw_model(self):
         pass
@@ -20,3 +25,21 @@ class Scene:
 
     def __clear_screen(self):
         pass
+
+    # Начало отрисовки окна и запуск цикла отлавливания событий
+    def show(self):
+        while True:
+            # Обработка событий
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.exit()
+                    exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.exit()
+                        exit()
+            # Очистка экрана
+            self.__window.fill((255, 255, 255))
+            # Отрисовка приколов
+            # ...
+            pygame.display.update()
