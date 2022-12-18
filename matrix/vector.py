8e714404c5dcd9c832c6a98299b058386f09a6a3
aastashov <aastashov@atollis.com>
2022-12-19 00:29:13 +0300

    Added class Vector

---
+++ matrix/vector.py
@@ -0,0 +1,42 @@
+from math import sqrt
+from typing import List, Union
+
+
+class Vector:
+
+    def __init__(self, vector_data: List[Union[float, int]]):
+        self.__x, self.__y, self.__z = vector_data
+
+    def set_x(self, x: float) -> "Vector":
+        self.__x = x
+        return self
+
+    def get_x(self) -> float:
+        return self.__x
+
+    def set_y(self, y: float) -> "Vector":
+        self.__y = y
+        return self
+
+    def get_y(self) -> float:
+        return self.__y
+
+    def set_z(self, z: float) -> "Vector":
+        self.__z = z
+        return self
+
+    def get_z(self) -> float:
+        return self.__z
+
+    # Вычисление длины вектора
+    def __abs__(self) -> float:
+        return sqrt(self.__x ** 2 + self.__y ** 2 + self.__z ** 2)
+
+    # Векторное произведение
+    def __mul__(self, other) -> "Vector":
+        if isinstance(other, Vector):
+            return Vector([self.get_y() * other.get_z() - self.get_z() * other.get_y(),
+                           self.get_z() * other.get_x() - self.get_x() * other.get_z(),
+                           self.get_x() * other.get_y() - self.get_y() * other.get_x()])
+        else:
+            raise ValueError
